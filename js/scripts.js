9d1d84475d573619ae7303e6173bd8d151d54e10
mtaut <146488331+mtaut@users.noreply.github.com>
2023-12-14 23:02:56 -0700

    Ex1.2 pokedex repository start
    Began JS code for pokedex repository app.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+let pokemanList = [
+    {
+      name: 'Charmander',
+      height: '0.6',
+      type: ['fire', 'monster', 'solar-power'],
+    },
+    {
+      name: 'Pikachu',
+      height: '0.4',
+      type: ['electric', 'fairy', 'static'],
+    },
+    {
+      name: 'Jigglypuff',
+      height: '0.5',
+      type: ['fairy', 'cute-charm', 'sing'],
+    },
+];
