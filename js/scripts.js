b94d93f6fb5c6f6721155de44027b1ea64221ea8
mtaut <146488331+mtaut@users.noreply.github.com>
2023-12-23 20:01:37 -0700

    Added (first attempt) for loop for pokemon height

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,14 @@
+// height is shown in inches
+
 let pokemanList = [
-    {
-      name: 'Charmander',
-      height: '24"',
-      type: ['fire', 'monster', 'solar-power'],
-    },
-    {
-      name: 'Pikachu',
-      height: '16"',
-      type: ['electric', 'fairy', 'static'],
-    },
-    {
-      name: 'Jigglypuff',
-      height: '20"',
-      type: ['fairy', 'cute-charm', 'sing'],
-    },
+    {name: 'Charmander', height: '24\"', type: ['fire',]},
+    {name: 'Pikachu', height: '16\"', type: ['electric']},
+    {name: 'Jigglypuff', height: '20\"', type: ['fairy']},
 ];
 
+// print message if height is taller than 20 (inches)
+if (pokemon.height > 20) {
+  document.write('<p>', pokemon.name + 'Wow, that\'s big!;' </p>');
+} else {
+  document.write('<p>', pokemon.name + pokemon.height + '; </p>');
+}
