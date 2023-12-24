dffa6e449f5a69da3c083458a7a9c1f128efbfec
mtaut <146488331+mtaut@users.noreply.github.com>
2023-12-23 19:04:20 -0700

    Updated height to inches.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
 let pokemanList = [
     {
       name: 'Charmander',
-      height: '0.6',
+      height: '24"',
       type: ['fire', 'monster', 'solar-power'],
     },
     {
       name: 'Pikachu',
-      height: '0.4',
+      height: '16"',
       type: ['electric', 'fairy', 'static'],
     },
     {
       name: 'Jigglypuff',
-      height: '0.5',
+      height: '20"',
       type: ['fairy', 'cute-charm', 'sing'],
     },
 ];
+
