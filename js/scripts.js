2ec710462117997caa340ccac0fbe1aff278a080
mtaut <146488331+mtaut@users.noreply.github.com>
2024-01-20 18:10:26 -0700

    Cleared older code.
    Deleted old code that had no purpose to simple JS app.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,10 @@
-//1st iteration
-/*if (pokemon.height > 20) {
-  document.write('<p>', pokemon.name + 'Wow, that\'s big!;' </p>');
-} else {
-  document.write('<p>', pokemon.name + pokemon.height + '; </p>');
-}*/
-
 // height is shown in inches
-//2nd iteration
+//added IIFE
 let pokemonList = [
   { name: "Charmander", height: 24, type: ["fire"]},
   { name: "Pikachu", height: 16, type: ["electric"]},
   { name: "Jigglypuff", height: 20, type: ["fairy"]}
-];
+]();
 
 // print message if height is taller than 20 (inches)
 //forEach function
