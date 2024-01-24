eee1a91976615910751f28e3eb4c956151d06105
mtaut <146488331+mtaut@users.noreply.github.com>
2024-01-23 22:29:20 -0700

    Styling pokemon list
    Added styling to pokemon repository. Practicing with DOM manipulation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,49 @@
 // height is shown in inches
 //added IIFE and new pokemonRepository
 let pokemonRepository = (function() {
-let pokemonList = [
+let pokemonRepository = [
   { name: "Charmander", height: 24, type: ["fire"], category: "Dragon"},
   { name: "Pikachu", height: 16, type: ["electric"], category: "Mouse"},
   { name: "Jigglypuff", height: 20, type: ["fairy"], category: "Balloon"}
 ]
-
-return {
-  add: function(pokemon) {
-    pokemonList.push(pokemon);
-  },
-  getAll: function(){
-    return pokemonList;
+function add(pokemon) {
+  if (
+    typeof pokemon === "object" &&
+    "name" in pokemon &&
+    "height" in pokemon &&
+    "types" in pokemon
+  ) {
+    repository.push(pokemon);
+  } else {
+    console.log("pokemon is not correct");
   }
-};
+}
+function getAll() {
+  return repository;
+}
+function addListItem(pokemon){
+  let pokemonList = document.querySelector(".pokemon-list");
+  let listpokemon = document.createElement('li');
+  let button = document.createElement("button");
+  button.innerText = pokemon.name; 
+  button.classList.add("button-class");
+  listpokemon.appendChild(button);
+  pokemonList.appendChild(listpokemon);
+}
+return {
+  add: add,
+  getAll: getAll,
+  addListItem: addListItem  
+  };
 })();
 
+pokemonRepository.add({ name: "Pikachu", height: 16, types: ["electric"] }); 
+
+console.log(pokemonRepository.getAll());
 
 //forEach function
-pokemonList.forEach(function(pokemon){
-  document.write(pokemon.name + ' is ' + pokemon.height + ' inches tall. Their pokemon type is ' + pokemon.type + '.');
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
 });
 
 //forLoop below here
