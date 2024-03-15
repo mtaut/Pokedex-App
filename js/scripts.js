3adc60d745942e5fc776f5840e1a3a34875a7f51
mtaut <146488331+mtaut@users.noreply.github.com>
2024-03-15 17:03:39 -0600

    Editing css, htm, js
    Editing code for spacing, as well as what I don't need.

--- js/scripts.js
+++ js/scripts.js
@@ -9,28 +9,19 @@ let pokemonRepository = (function () {
   function showModal(item) {
     let modalBody = $(".modal-body");
     let modalTitle = $(".modal-title");
-    let modalHeader = $(".modal-header");
-    
+        
     modalTitle.empty();
     modalBody.empty();
 
   // elements to display pokemon details: pokemon name + front & back image + height + weight + types + abilities
     let nameElement = $("<h1>" + item.name + "</h1>");
-
-    let imageElementFront = $('img class="modal-img" style="width:50%">');
-    imageElementFront.attr("src", item.imageUrlFront);
-
-    let imageElementBack = $('<img class="modal-img" style="width:50%">');
-    imageElementBack.attr("src", item.imageUrlBack);
-
+    let imageElementFront = $('img class="modal-img" style="width:50%">').attr("src", item.imageUrlFront);
+    let imageElementBack = $('<img class="modal-img" style="width:50%">').attr("src", item.imageUrlBack);
     let heightElement = $("<p>" + "height: " + item.height + "</p>");
-
     let weightElement = $("<p>" + "weight: " + item.weight + "</p>");
-
     let typesElement = $("<p>" + "types: " + item.types + "</p>");
-
     let abilitiesElement = $("<p>" + "abilities: " + item.abilities + "</p>");
-         
+
     modalTitle.append(nameElement);
     modalBody.append(imageElementFront);
     modalBody.append(imageElementBack);
@@ -42,94 +33,10 @@ let pokemonRepository = (function () {
    
     modalContainer.innerHTML = ''; 
     modalContainer.appendChild(modal);    
-    modalContainer.classList.add('is-visible');      
+    modalContainer.classList.add('is-visible'); 
+    $('#apiUrl').modal('show');     
   }     
   
-  function hideModal() {
-    modalContainer.classList.remove('is-visible');
-  }
-  
-  window.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-      hideModal();
-    }
-  });
-
-  // Struggling to get modal to close when I click outside of modal...
-  /*modalContainer.addEventListener('click', (e) => {
-    let target = e.target;
-    if (target === modalContainer) {
-      hideModal();
-    }
-  });*/
-
-  
-  //ex1.6, DOM interation here
-  function add(pokemon) {
-    if (typeof pokemon === "object" && "name" in pokemon) {
-      pokemonList.push(pokemon);
-    } else {
-      console.log("pokemon is not correct");
-    }
-  }
-  function getAll() {
-    return pokemonList;
-  } 
-  //from ex1.6, DOM interaction
-  function addListItem(pokemon) {
-    let pokemonListElement = document.querySelector(".pokemon-list");
-    let listItem = document.createElement("li");
-    let button = document.createElement("button");
-    button.innerText = pokemon.name;
-    button.classList.add("button-class");
-    listItem.appendChild(button);
-    pokemonListElement.appendChild(listItem);
-    button.addEventListener("click", function (event) {
-      showDetails(pokemon);
-    });
-  } 
-  //loadList() function here, from ex1.7
-  /*function loadList() {
-    return fetch(apiUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        data.results.forEach(function (item) {
-          let pokemon = {
-            name: item.name,
-            detailsUrl: item.url,
-          };
-          add(pokemon);          
-        });
-      })
-      .catch(function (e) {
-        console.error(e);
-      });
-  } 
-  //loadDetails() function here, from ex1.7
-  function loadDetails(item) {    
-    let url = item.detailsUrl;
-    return fetch(url)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (details) {
-        //Here, details are added to the item
-        item.imageUrlFront = details.sprites.front_default;
-        item.height = details.height;                
-      })
-      .catch(function (e) {
-        console.error(e);        
-      });
-  } */
-
-  //showDetails() function, to show modal w/details of a Pokemon 
-  /*function showDetails(item) {
-    loadDetails(item).then(function () {
-      showModal(item);
-    });
-  }
   return {
     add: add,
     getAll: getAll,
@@ -138,18 +45,104 @@ let pokemonRepository = (function () {
     loadDetails: loadDetails,
     showDetails: showDetails,
   };
-})();*/
+})();
+
+function addListItem(pokemon) {
+  let pokemonListElement = document.querySelector(".pokemon-list");
+  let listItem = document.createElement("li");
+  let button = document.createElement("button");
+  button.innerText = pokemon.name;
+  button.classList.add("button-class");
+  listItem.appendChild(button);
+  pokemonListElement.appendChild(listItem);
+  button.addEventListener("click", function (event) {
+    showDetails(pokemon);
+  });
+} 
+//loadList() function here, from ex1.7
+function loadList() {
+  return fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      data.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url,
+        };
+        add(pokemon);          
+      });
+    })
+    .catch(function (e) {
+      console.error(e);
+    });
+}   
+//loadDetails() function here, from ex1.7
+function loadDetails(item) {    
+  let url = item.detailsUrl;
+  return fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (details) {
+      //Here, details are added to the item
+      item.imageUrlFront = details.sprites.front_default;
+      item.height = details.height;                
+    })
+    .catch(function (e) {
+      console.error(e);        
+    });
+} 
+
+//showDetails() function, to show modal w/details of a Pokemon 
+function showDetails(item) {
+  loadDetails(item).then(function () {
+    showModal(item);
+  });
+}
 
 //console.log(pokemonRepository.getAll());
 
-/*pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
-});
+pokemonRepository.getAll().forEach(function (pokemonList) {
+pokemonRepository.addListItem(pokemon);
+}); 
 
 //loadList() function here, from ex1.7
 pokemonRepository.loadList().then(function () {
-  //Now the data is loaded
-  pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon);
-  });*/
+//Now the data is loaded
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
 });
+});
+
+  /*function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+  
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });*/
+
+  // Struggling to get modal to close when I click outside of modal...
+  /*modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });*/
+  
+  //ex1.6, DOM interation here
+  /*  function add(pokemon) {
+    if (typeof pokemon === "object" && "name" in pokemon) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
+  }
+  function getAll() {
+    return pokemonList;
+  }   */
+  //from ex1.6, DOM interaction
